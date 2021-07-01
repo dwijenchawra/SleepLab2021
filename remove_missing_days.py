42bc8e679cf3af8c544b492168d94edb2b8c0858
Dwijen Chawra <dwijenchawra@yahoo.com>
2021-07-01 14:13:46 -0700

    added more data into parseable

--- remove_missing_days.py
+++ remove_missing_days.py
@@ -4,6 +4,9 @@ IMPUTE_CSV = "Andrew_M-timeSeries.csv"
 
 # 2021-05-03 16:12:49.619000+0100 [Europe/London]
 
+def revertTime(year, month, day, hour, minute, second):
+    return f"{year}-{month}-{day} {hour}:{minute}:{second}.619000+0100 [Europe/London]"
+
 df = pd.read_csv(IMPUTE_CSV)
 
 timestamps = df['time']
@@ -11,23 +14,31 @@ imputed = df['imputed']
 
 zipdict = dict(zip(timestamps, imputed))
 
-parseable = [[int(str(timestamps[i]).split(" ")[0].split("-")[2]), imputed[i]] for i in range(len(timestamps))]
+parseable = [
+    [list(map(int, str(timestamps[i]).split(" ")[0].split("-"))),
+     list(map(int, str(timestamps[i]).split(" ")[1].split(".")[0].split(":"))),
+     imputed[i]]
+    for i in range(len(timestamps))]
 
 final = []
 
-prev = parseable[0][0]
+prev = parseable[0][0][2]
 sum = 0
 count = 0
 
 for i in range(0, len(parseable)):
     count += 1
-    if parseable[i][0] != prev:
-        prev = parseable[i][0]
+    if parseable[i][0][2] != prev:
+        prev = parseable[i][0][2]
         ratio = sum / count
         if ratio < 0.5:
             final.extend(parseable[i - (count - 1):i])
+            #todo add df values to new df
         sum = 0
         count = 0
-    sum += parseable[i][1]
+    sum += parseable[i][2]
 
+def revertList(data):
+    return [[revertTime(i[0][0], i[0][1], i[0][2], i[1][0], i[1][1], i[1][2]), i[2]] for i in data]
 
+export = revertList(final)
