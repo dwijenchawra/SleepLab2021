c8022567ee7bd8be18543a34616b3ab01d8c7d9d
Dwijen Chawra <dwijenchawra@yahoo.com>
2021-07-13 12:06:46 -0700

    modified for use in function

--- ggir_csv_prep_for_nparact.py
+++ ggir_csv_prep_for_nparact.py
@@ -1,10 +1,9 @@
-import time
-
 import pandas as pd
 
 
 # df = pd.read_csv("1001787_90001_0_0.cwa.RData.csv")
-def convertForNPARact(path):
+def convertForNPARact(path: str, destination: str):
+    filename = path.split("\\")[-1]
     df = pd.read_csv(path)
 
     for i in range(len(df["timestamp"])):
@@ -15,7 +14,5 @@ def convertForNPARact(path):
 
     # 2014-05-29T10:15:00-0700
     df = df.drop(["ENMO"], axis=1)
-    # print(df)
-    # print(df.isnull().sum().sum())
-    # df.to_csv("nparacttest/ggir_imputed.csv", header=False, index=False)
-    df.to_csv("nparacttest/python_imputed.csv", header=False, index=False)
+
+    df.to_csv(destination + "//" + "nparactformat_" + filename, header=False, index=False)
